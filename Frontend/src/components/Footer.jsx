20304a0eb0551982ad4118975c5b73fca6792e1f
sanjaykumardev <duderaj1976@gmail.com>
2024-05-14 09:50:15 +0530

    change

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -5,21 +5,25 @@ const Footer = () => {
   return (
     <>
       <div className="  w-full bg-blue-500 px-8 md:px-[400px] md:flex-row p-10 flex-col flex  space-y-0 items-start justify-between sm:text-sm md:text-md py-20 md:text-1xl sm:space-x-0 gap-4">
-        <div className=" flex flex-col  gap-3 text-2xl text-white">
+        <div className=" flex flex-col  gap-3 text-xl text-white">
+          <h1>Customer Service</h1>
           <p>Fearture Blogs</p>
           <p>Most Viewed</p>
+          <p>Delivery Department</p>
+          <p>Delivery Department</p>
+          <p>Delivery Department</p>
           <p>Delivery Department</p>
         </div>
 
         {/* middle */}
 
-        <div className=" flex flex-col gap-3 text-2xl text-white ">
+        <div className=" flex flex-col gap-3 text-xl text-white ">
           <p> Forum</p>
           <p>Support</p>
           <p>Logistic Support</p>
         </div>
 
-        <div className=" flex flex-col gap-3  text-2xl text-white">
+        <div className=" flex flex-col gap-3  text-xl text-white">
           <p>Privacy Policy</p>
           <p>About Us </p>
           <p>Terms & Condition</p>
